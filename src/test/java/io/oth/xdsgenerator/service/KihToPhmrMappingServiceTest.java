32d98f34c14cf2c000ba26755ca1c0f7daa4f322
Simon Walenkamp <simonwalenkamp@hotmail.com>
2023-06-09 14:12:48 +0200

    make test fail

--- src/test/java/io/oth/xdsgenerator/service/KihToPhmrMappingServiceTest.java
+++ src/test/java/io/oth/xdsgenerator/service/KihToPhmrMappingServiceTest.java
@@ -89,6 +89,7 @@ class KihToPhmrMappingServiceTest {
             thrown = e;
         }
 
+        assertTrue(false);
         // Then
         assertNotNull(thrown);
         assertTrue(thrown.getMessage().contains("Citizen skal være sat"));
